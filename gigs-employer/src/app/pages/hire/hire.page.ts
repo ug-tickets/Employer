1d87369b5b1a31aeebe03445917de401096a0b8d
kdenno <kabuyedennis3@gmail.com>
2020-01-03 21:12:50 +0300

    create hire page

--- gigs-employer/src/app/pages/hire/hire.page.ts
+++ gigs-employer/src/app/pages/hire/hire.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { PaymodesComponent } from 'src/app/paymodes/paymodes.component';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-hire',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hire.page.scss'],
 })
 export class HirePage implements OnInit {
+  
+  @ViewChild('payoverlay', {static: false}) payoverlay: ElementRef;
+  private paymcomp: PaymodesComponent;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onSubmit(f: NgForm) {
+    const paymode = document.querySelector('.paymodeoverlay');
+    paymode.classList.add('is-visible');
+
   }
 
 }
--- gigs-employer/src/app/paymodes/paymodes.component.ts
+++ gigs-employer/src/app/paymodes/paymodes.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-paymodes',
   templateUrl: './paymodes.component.html',
   styleUrls: ['./paymodes.component.scss'],
 })
-export class PaymodesComponent implements OnInit {
+export class PaymodesComponent implements OnInit, AfterViewInit {
+  issearching = false;
+  theoverlay: any;
+  @ViewChild('payoverlay', { static: false }) payoverlay: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private router: Router) { }
 
   ngOnInit() {}
+  ngAfterViewInit() {
+    this.theoverlay = this.payoverlay.nativeElement;
+    this.renderer.listen(this.theoverlay, 'click', (e) => {
+      if (e.target.classList.contains('paymodeoverlay')) {
+        this.renderer.removeClass(this.theoverlay, 'is-visible');
+        this.issearching = false;
+      }
+
+
+    });
+
+  }
+  checking() {
+    this.issearching = !this.issearching;
+    // simulate http request
+    setTimeout(() => {
+      this.issearching = false;
+      this.renderer.removeClass(this.theoverlay, 'is-visible');
+      this.router.navigateByUrl('/searchedhustlers');
+
+    }, 3000);
+  }
 
 }
