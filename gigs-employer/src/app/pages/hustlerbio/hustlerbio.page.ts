c419265e653ff6a8e4e4ab32be879b388c71ea5a
kdenno <kabuyedennis3@gmail.com>
2020-01-07 19:46:09 +0300

    change hustler bio page navs to divs

--- gigs-employer/src/app/pages/hustlerbio/hustlerbio.page.ts
+++ gigs-employer/src/app/pages/hustlerbio/hustlerbio.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 
@@ -11,22 +11,46 @@ export class HustlerbioPage implements OnInit, AfterViewInit {
 
   constructor(private sanitization: DomSanitizer, private router: Router, private elem: ElementRef, private renderer: Renderer2) { }
   hustlerdp = '../../assets/img/deno.jpg';
-  currentSelection: String;
+  @ViewChild('currentSelection', { static: false }) currentSelection: ElementRef;
 
   ngOnInit() {
   }
-  ngAfterViewInit() {
-    this.currentSelection = 'About';
+  ngAfterViewInit() { 
+    //TODO: Change html navs to div coz divs will be easier to work with
+    this.currentSelection.nativeElement.innerHTML = 'About';
+
     const thecollection = this.elem.nativeElement.querySelectorAll('.nze');
     Array.from(thecollection).forEach((ele: HTMLElement) => {
       this.renderer.listen(ele, 'click', (e) => {
         // get parent element
-        const parentNode = this.renderer.parentNode(e.target);
+        let parentNode;
+        if(e.target.className="nze") {
+          parentNode = this.renderer.parentNode(e.target);
+
+        }
+        if (e.target.parentNode.classList.contains('nze')) {
+          // continue one level up
+          parentNode = this.renderer.parentNode(e.target.parentNode); 
+
+        }
+        
         Array.from(parentNode.children).forEach((elem: HTMLElement) => {
-          this.renderer.removeClass(elem, 'sorry');
+          this.renderer.removeClass(elem, 'deno');
         });
-        this.renderer.addClass(e.target, 'deno');
-        this.currentSelection = e.target.innerText;
+        if (e.target.className == 'nze') { // he has clicked the li its self
+         // this.renderer.addClass(e.target, 'deno');
+         e.target.classList.add('deno');
+          
+
+        }
+        if (e.target.parentNode.classList.contains('nze')) { // he has clicked on the divs inside the li
+         // this.renderer.addClass(e.target.parentNode, 'deno');
+         //e.target.parentNode.classList.add('deno');
+
+        };
+
+
+        this.renderer.setProperty(this.currentSelection.nativeElement, 'innerHTML', e.target.innerText);
       });
     });
   }
