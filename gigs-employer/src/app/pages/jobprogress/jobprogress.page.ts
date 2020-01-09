915298952f4470073cab0f1e680143fee88389df
kdenno <kabuyedennis3@gmail.com>
2020-01-09 14:35:39 +0300

    add progress bars

--- gigs-employer/src/app/pages/jobprogress/jobprogress.page.ts
+++ gigs-employer/src/app/pages/jobprogress/jobprogress.page.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jobprogress.page.scss'],
 })
 export class JobprogressPage implements OnInit {
-
+  extensionheader: string = 'Extend Gig By';
+  confirmit =false;
   constructor() { }
 
   ngOnInit() {
+  }
+  onSelect(event: CustomEvent) {
+    // console.log(event);
+    
+    this.confirmit = true;
+
+  }
+  onConfirm() {
+    this.extensionheader = 'Extended by';
+    this.confirmit = false;
+
   }
 
 }
